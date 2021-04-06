527f5fbb31b0b0de1451f32c49177ce0d9e188c0
Branden Adams <Branden.Adams@colorado.edu>
2021-04-06 13:24:38 -0600

    "Hopefully added ability to send different commands without exiting script functionality"

--- Lab4_2.py
+++ Lab4_2.py
@@ -14,7 +14,7 @@ os.environ['DISPLAY'] = ':0'
 frameWidth = 640
 frameHeight = 480
 
-ipt = []
+
 #Establishes serial connection with the Arduino before the code can be run
 if __name__ == '__main__':
 	ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
@@ -68,86 +68,94 @@ def getContours(img,imgContour):
 			cv2.putText(imgContour, "Area: " + str(int(area)), (x + w + 20, y + 45), cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
 			b = True
 			#return b
+def comnd(ipt):
+	while ipt == []:
+		ipt = input("Please Enter a Command: ")
+		if ipt == "list":
+			print("List of Commands:   ")
+			print("Command:			What it Do:		Arduino Gets:")
+			print("")
+			print("F				  Forward	 	      70")
+			print("B		    	   Back			      66")
+			print("r     			  90deg CW 	         114")
+			print("Q  			     180deg CW 			  82")
+			print("L  				 FWD Line 			  76")
+			ipt = []
+		else:
+			ipt = list(ipt)
+			ipt = ipt[1]
+		print("Sending:   ",ipt)
+		ser.write(str(ipt).encode('utf-8'))
 
-while ipt == []:
-	ipt = input("Please Enter a Command: ")
-	if ipt == "list":
-		print("List of Commands:   ")
-		print("Command:			What it Do:		Arduino Gets:")
-		print("")
-		print("F				  Forward	 	      70")
-		print("B		    	   Back			      66")
-		print("r     			  90deg CW 	         114")
-		print("Q  			     180deg CW 			  82")
-		print("L  				 FWD Line 			  76")
-		ipt = []
 
-	ser.write(char(ipt).encode('utf-8'))
 # Main loop of the script
-while ipt ~= [] and ipt ~= "list":
-	_,frame = cap.read()
-	#print(len(frame))
-	imgContours = frame.copy()
 
-	# Thresholds for contour stuff (unused as of now I think)
-	threshold1 = cv2.getTrackbarPos("T1: ","Thresholds")
-	threshold2 = cv2.getTrackbarPos("T2: ","Thresholds")
+while True:
+	ipt = []
+	comnd(ipt)
+	while ipt != [] and ipt != list:
+		_,frame = cap.read()
+		#print(len(frame))
+		imgContours = frame.copy()
 
-	# Higher and lower thresholds of the HSV values
-	lg = np.array([cv2.getTrackbarPos("H_LOW ","Parameters"),cv2.getTrackbarPos("S_LOW ","Parameters"),cv2.getTrackbarPos("V_LOW ","Parameters")])
-	ug = np.array([cv2.getTrackbarPos("H_HIGH ","Parameters"),cv2.getTrackbarPos("S_HIGH ","Parameters"),cv2.getTrackbarPos("V_HIGH ","Parameters")])
-	hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+		# Thresholds for contour stuff (unused as of now I think)
+		threshold1 = cv2.getTrackbarPos("T1: ","Thresholds")
+		threshold2 = cv2.getTrackbarPos("T2: ","Thresholds")
 
-	# imgBlue is a blured version of the original hsv masked image such that
-	# higher quality contours can be drawn around the ballons
-	imgBlur = cv2.GaussianBlur(hsv,(7,7),1)
+		# Higher and lower thresholds of the HSV values
+		lg = np.array([cv2.getTrackbarPos("H_LOW ","Parameters"),cv2.getTrackbarPos("S_LOW ","Parameters"),cv2.getTrackbarPos("V_LOW ","Parameters")])
+		ug = np.array([cv2.getTrackbarPos("H_HIGH ","Parameters"),cv2.getTrackbarPos("S_HIGH ","Parameters"),cv2.getTrackbarPos("V_HIGH ","Parameters")])
+		hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
 
-	# mask is the masked image of the blured hsv that seeks values of h,s,and v
-	# within the specified bounds
-	mask = cv2.inRange(imgBlur, lg, ug)
+		# imgBlue is a blured version of the original hsv masked image such that
+		# higher quality contours can be drawn around the ballons
+		imgBlur = cv2.GaussianBlur(hsv,(7,7),1)
 
-	#print(len(mask))
+		# mask is the masked image of the blured hsv that seeks values of h,s,and v
+		# within the specified bounds
+		mask = cv2.inRange(imgBlur, lg, ug)
 
-	#imgGray and Canny are unused currently, haven't figured out if this is
-	#Something we would like to pursue in the future.
-	imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
-	imgCanny = cv2.Canny(imgGray,threshold1,threshold2)
-	kernel = np.ones((5,5))
-	imgDil = cv2.dilate(mask, kernel, iterations = 1)
-	b = False
-	getContours(imgDil, imgContours)
+		#print(len(mask))
+
+		#imgGray and Canny are unused currently, haven't figured out if this is
+		#Something we would like to pursue in the future.
+		imgGray = cv2.cvtColor(imgBlur, cv2.COLOR_BGR2GRAY)
+		imgCanny = cv2.Canny(imgGray,threshold1,threshold2)
+		kernel = np.ones((5,5))
+		imgDil = cv2.dilate(mask, kernel, iterations = 1)
+		b = False
+		getContours(imgDil, imgContours)
 
 
-#	Image outputs for the pure image, the masked image, and the contours
+	#	Image outputs for the pure image, the masked image, and the contours
 
-	cv2.imshow("Frame: ",frame)
-	cv2.imshow("Mask: ",mask)
-	cv2.imshow("Contours: ", imgContours)
-	print(b)
-	#print(area)
-	if (b == True and area < 100000):
-		#print(cent)
-		if (cent < 80):
-			ser.write(str(2).encode('utf-8'))
-			#sleep(.4)
-			print("left")
-		if (cent > 510):
-			ser.write(str(3).encode('utf-8'))
-			print("right")
-			#sleep(.4)
-		if (cent >=  80 and cent <= 510):
-			ser.write(str(1).encode('utf-8'))
-			print("straight")
-			#sleep(.4)
-	else:
-		ser.write(str(0).encode('utf-8'))
+		cv2.imshow("Frame: ",frame)
+		cv2.imshow("Mask: ",mask)
+		cv2.imshow("Contours: ", imgContours)
+		print(b)
+		#print(area)
+		if (b == True and area < 100000):
+			#print(cent)
+			if (cent < 80):
+				ser.write(str(2).encode('utf-8'))
+				#sleep(.4)
+				print("left")
+			if (cent > 510):
+				ser.write(str(3).encode('utf-8'))
+				print("right")
+				#sleep(.4)
+			if (cent >=  80 and cent <= 510):
+				ser.write(str(1).encode('utf-8'))
+				print("straight")
+				#sleep(.4)
+		else:
+			ser.write(str(0).encode('utf-8'))
 
-	# Command to read back what was sent to the Arduino
-	if ser.in_waiting >0:
-		line = ser.readline().decode('utf-8').rstrip()
-		print(line)
-	# If this if statement is removed, the output will be just a still Image
-	# not a video
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
-			#print(x+(w/2))
+		# Command to read back what was sent to the Arduino
+		if ser.in_waiting >0:
+			line = ser.readline().decode('utf-8').rstrip()
+			print(line)
+		# If this if statement is removed, the output will be just a still Image
+		# not a video
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
